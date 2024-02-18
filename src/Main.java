5bde5084c8c2e51eb92358ee59f31ab9ca80ae1c
AngelsGV <angels.not99@gmail.com>
2024-02-18 21:59:11 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 import java.util.List;
-
+//Se que puedo hacerlo mas elegante poniendo scanner y hacíendo un logaritmo que te pida el numero de elementos
+// de la cola inicial o los elementos iniciales y los que quieres introducir después.
+//Pero me he centrado en lo que pedía el ejercicio y ya, lo otro lo tengo más claro y
+// no me quiero complicar en esa parte del código.
 public class Main {
 public static void main(String[] args) {
         Cola c1 = new Cola();
